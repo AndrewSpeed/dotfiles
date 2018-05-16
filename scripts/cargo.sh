f2b658517e655f81a1ea330146b38b351d2b6fa9
Andrew Speed <andrew.cspeed@gmail.com>
2018-05-16 22:24:58 +0100

    Add script files for cargo and go env variables

---
+++ scripts/cargo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# shellcheck source=/home/aspeed/.cargo/bin disable=SC1091
+export CARGO_BIN_PATH="$HOME/.cargo/bin"
---
+++ scripts/go.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export GOROOT="/usr/local/go"
+export GOPATH="$HOME/projects/Go"
+export GOROOT_BIN="$GOROOT/bin"
+export GOPATH_BIN="$GOPATH/bin"
