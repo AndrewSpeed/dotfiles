37de652961e6fb2d8e90e638316e4741357d3b05
Andrew Speed <2609244+AndrewSpeed@users.noreply.github.com>
2024-11-21 22:37:15 +0000

    Add conform plugin

--- shell/.config/nvim/init.lua
+++ shell/.config/nvim/init.lua
@@ -225,6 +225,20 @@ require("lazy").setup({
       })
     end
   },
+  -- code formatter
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt", lsp_format="fallback" }
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  },
   -- language support
   -- terraform
   {
