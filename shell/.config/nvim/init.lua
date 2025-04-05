8be27ef1c5a7ba12b707be52a9ea55611d4741c1
Andrew Speed <andrew.cspeed@gmail.com>
2025-04-05 23:07:22 +0100

    Add plugins for local config and pytest

--- shell/.config/nvim/init.lua
+++ shell/.config/nvim/init.lua
@@ -92,6 +92,33 @@ require("lazy").setup({
 
   "junegunn/fzf",
 
+  -- project specific config
+  {
+    "klen/nvim-config-local",
+    config = function()
+      require('config-local').setup {
+        -- Default options (optional)
+
+        -- Config file patterns to load (lua supported)
+        config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+
+        -- Where the plugin keeps files data
+        hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+        autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+        commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalDeny)
+        silent = false,             -- Disable plugin messages (Config loaded/denied)
+        lookup_parents = false,     -- Lookup config files in parent directories
+      }
+    end
+  },
+
+  -- pytest
+  {
+    "richardhapb/pytest.nvim",
+    opts = {},
+  },
+
   -- LSP
   {
     'neovim/nvim-lspconfig',
@@ -325,3 +352,5 @@ require("lazy").setup({
     end
   },
 })
+
+require('pytest').setup()
