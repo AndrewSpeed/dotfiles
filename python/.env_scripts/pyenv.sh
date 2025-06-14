9a00b6b056b052dd840039283775603bea47ea31
Andrew Speed <andrew.cspeed@gmail.com>
2025-06-14 21:25:39 +0100

    Make pyenv config conditional

--- python/.env_scripts/pyenv.sh
+++ python/.env_scripts/pyenv.sh
@@ -4,7 +4,12 @@ export PYENV_ROOT="$HOME/.pyenv"
 
 # Load pyenv automatically:
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
 
-# Load pyenv-virtualenv automatically:
-eval "$(pyenv virtualenv-init -)"
+if [[ "$(whence pyenv)" > /dev/null ]]; then
+    eval "$(pyenv init -)"
+
+    # Load pyenv-virtualenv automatically:
+    eval "$(pyenv virtualenv-init -)"
+else
+    echo "pyenv not installed, skipping configuration"
+fi
