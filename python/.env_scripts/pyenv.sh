72393219bd795b8add828932f17ae89773c08868
Andrew Speed <andrew.cspeed@gmail.com>
2025-07-14 15:01:43 +0100

    Make pyenv loading configurable via env var

--- python/.env_scripts/pyenv.sh
+++ python/.env_scripts/pyenv.sh
@@ -5,11 +5,15 @@ export PYENV_ROOT="$HOME/.pyenv"
 # Load pyenv automatically:
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 
-if [[ "$(whence pyenv)" > /dev/null ]]; then
-    eval "$(pyenv init -)"
-
-    # Load pyenv-virtualenv automatically:
-    eval "$(pyenv virtualenv-init -)"
+if [[ -z "$DISABLE_PYENV" ]] || [[ "$DISABLE_PYENV" -eq 0 ]]; then
+    if [[ "$(whence pyenv)" > /dev/null ]]; then
+        eval "$(pyenv init -)"
+    
+        # Load pyenv-virtualenv automatically:
+        eval "$(pyenv virtualenv-init -)"
+    else
+        echo "pyenv not installed, skipping configuration"
+    fi
 else
-    echo "pyenv not installed, skipping configuration"
+    echo "Skipping pyenv init due to DISABLE_PYENV"
 fi
