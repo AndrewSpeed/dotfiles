0ac14adf356daaa5eef2b08300c82974eb2c2298
Andrew Speed <andrew.cspeed@gmail.com>
2025-08-02 12:33:55 +0100

    Remove autojump config, replaced with zoxide

--- shell/.env_scripts/autojump.sh
+++
@@ -1,9 +0,0 @@
-if [[ "$(whence autojump)" > /dev/null ]]; then
-    # *nix: load appropriate shellscript for our shell
-    [ -f /usr/share/autojump/autojump.$(basename "$SHELL") ] && . /usr/share/autojump/autojump.$(basename "$SHELL")
-
-    # macos
-    [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh # load autojump
-else
-    echo "Autojump not installed, skipping configuration"
-fi
